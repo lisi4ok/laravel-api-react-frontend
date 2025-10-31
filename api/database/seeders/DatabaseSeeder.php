16724030ab5d75add8db3c5abc3ada6a0cd440e8
Zaio Klepoyshkov <lisi4ok@gmail.com>
2025-10-31 14:45:33 +0200

    Fix seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Interaction;
+use App\Models\Contact;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
